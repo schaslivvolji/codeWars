eefd06b2d29dc13927929916f954762c1bbbdb20
schaslivvolji <schaslivvolji16@gmail.com>
2023-07-19 18:10:47 +0500

    onehandritbaks

---
+++ 8kay/Welcome!.js
@@ -0,0 +1,26 @@
+function greet(language){
+    let dataBaseLanguage = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+    }
+        if (language in dataBaseLanguage){
+            return dataBaseLanguage[language]
+        } else {
+        return dataBaseLanguage.english
+        }
+    }
